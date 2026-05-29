25fcfef05f4c41a04b1a0d84a32770fadb8ee68d
abdair-coca <cocaabdair@gmail.com>
2026-05-28 21:39:21 -0400

    Parte 2: configurar modelos y relaciones

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
-    /** @use HasFactory<\Database\Factories\CategoriaFactory> */
-    use HasFactory;
+    protected $table = 'categorias';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'activo'
+    ];
+
+    protected $casts = [
+        'activo' => 'boolean',
+    ];
+
+    public function productos(): HasMany
+    {
+        return $this->hasMany(Producto::class);
+    }
 }
--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -2,11 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Producto extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductoFactory> */
-    use HasFactory;
+    protected $table = 'productos';
+
+    protected $fillable = [
+        'categoria_id',
+        'nombre',
+        'sku',
+        'precio',
+        'stock',
+        'disponible'
+    ];
+
+    protected $casts = [
+        'precio' => 'decimal:2',
+        'disponible' => 'boolean',
+    ];
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
